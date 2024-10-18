82c5b857482a89ed8d09c97cadc531e809b408cb
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-19 00:15:00 +0900

    feat: 서스펜스 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import TodoContent from "./components/TodoContent/TodoContent";
 import "./App.css";
+import { lazy, Suspense } from "react";
+const TodoContent = lazy(() => import('./components/TodoContent/TodoContent'));
 
 function App() {
   return (
     <div className="container">
       <header>Todo app</header>
       <main>
-          <TodoContent/>
+          <Suspense fallback={<div className="todo-content-fallback">할 거 기억하는 중..</div>}>
+            <TodoContent/>
+          </Suspense>
       </main>
     </div>
   );
