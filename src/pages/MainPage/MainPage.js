ebebbdbcb280299c76371edd7d1ed4a20daa5e41
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-22 00:36:01 +0900

    feat: 로그인 버튼 만들기

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,13 +1,19 @@
 import { lazy, Suspense } from "react";
+import { Link } from "react-router-dom";
 const TodoContent = lazy(() => import("../../components/TodoContent/TodoContent"));
 
 function MainPage() {
     return (
         <>
-            <header>Todo app</header>
             <main>
+                <header>
+                    <h1>Todo app</h1>
+                    <Link to={'/login'}>
+                        <button className="btn">로그인</button>
+                    </Link>
+                </header>
                 <Suspense fallback={<div className="todo-content-fallback">할 거 기억하는 중..</div>}>
-                <TodoContent/>
+                    <TodoContent/>
                 </Suspense>
             </main>
         </>
