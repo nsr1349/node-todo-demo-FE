3b7e67eb26113c37acb397cf62dfdf8242f6af0e
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-23 23:04:30 +0900

    feat: 라우팅 로직 반영

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { getUserApi } from "./utils/user";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SiginUpPage from "./pages/SiginUpPage/SignUpPage";
 import NotFound from "./components/NotFound";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import PrivateRoute from "./components/PrivateRoute.js/PrivateRoute";
+import "./App.css";
 
 function App() {
+  const [ user , setUser ] = useState(true)
+
+  const getUser = async () => {
+    const { status , data } = await getUserApi()
+    if (status === 200) return setUser(data.user)
+    setUser(null)
+  }
+
+  useEffect(()=> {
+    getUser()
+  },[])
+
   return (
     <div className="container">
         <BrowserRouter>
             <Routes>
-              <Route path="/" element={<MainPage />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
-              <Route path="/signUp" element={<SiginUpPage />}></Route>
-              <Route path="*" element={<NotFound />}></Route>
+              <Route path="/" element={
+                <PrivateRoute logic={user} to={'/login'}>
+                  <MainPage setUser={setUser}/>
+                </PrivateRoute>
+              }/>
+              <Route path="/login" element={
+                <PrivateRoute logic={!user} to={'/'}>
+                  <LoginPage user={user} setUser={setUser} />
+                </PrivateRoute>}/>
+              <Route path="/signUp" element={<SiginUpPage />}/>
+              <Route path="*" element={<NotFound />}/>
             </Routes>
         </BrowserRouter>
     </div>
